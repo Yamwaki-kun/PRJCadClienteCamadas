285522e664b8987cf54311359215c56eb03237a1
Arnaldo Victor Yamawaki Alves <arnaldoalves34@gmail.com>
2019-05-14 15:25:26 -0300

    Criação do pacote Persistencia e da classe CRUDCliente com os metodos sem corpo

---
+++ src/com/prjcadcliente/persistencia/CRUDCliente.java
@@ -0,0 +1,40 @@
+package com.prjcadcliente.persistencia;
+
+import java.util.List;
+
+import com.prjcadcliente.dominio.Cliente;
+
+/**
+ * <b>CRUDCliente</b><br>
+ * Essa Classe permite manipular as informações do cliente. Aqui você
+ * encontrará os seguintes comandos:
+ * <ul>
+ * <li>Cadastro</li>
+ * <li>Pesquisar por nome e por id</li>
+ * <li>Atualizar</li>
+ * <li>Deletar</li>
+ * </ul>
+ * @author arnaldo.vyalves
+ *
+ */
+public class CRUDCliente {
+	public String cadastrar(Cliente cliente) {
+		return null;
+	}
+	public String atualizar(Cliente cliente) {
+		return null;
+	}
+	public String deletar(Cliente cliente) {
+		return null;
+	}
+	public List<Cliente> PesquisarPorNome(String nome){
+		return null;
+	}
+	public Cliente PesquisarPorId(int id){
+		return null;
+	}
+	
+	public List<Cliente> PesquisarTodos(){
+		return null;
+	}
+}
