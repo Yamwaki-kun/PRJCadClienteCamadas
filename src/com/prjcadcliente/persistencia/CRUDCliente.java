59bbfbf725d30f21c48b40bdb4a46dc9073db5c0
Arnaldo Victor Yamawaki Alves <arnaldoalves34@gmail.com>
2019-05-16 16:56:23 -0300

    Adicionado funçoes dos botoes deletar, criar e atualizar

--- src/com/prjcadcliente/persistencia/CRUDCliente.java
+++ src/com/prjcadcliente/persistencia/CRUDCliente.java
@@ -37,7 +37,7 @@ public class CRUDCliente {
 		
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
-			con = DriverManager.getConnection("jdbc:mysql://localhost:3307/clientedb","root","");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/clientedb?serverTimezone=UTC","root","");
 		
 			String consulta = "INSERT INTO tbclientes(nome,email,telefone,idade)values(?,?,?,?)";
 			
@@ -73,7 +73,7 @@ public class CRUDCliente {
 		
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
-			con = DriverManager.getConnection("jdbc:mysql://localhost:3307/clientedb","root","");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/clientedb?serverTimezone=UTC","root","");
 		
 			String consulta = "UPDATE tbclientes set nome=?,email=?,telefone=?,idade=? WHERE id=?";
 			
@@ -110,7 +110,7 @@ public class CRUDCliente {
 		
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
-			con = DriverManager.getConnection("jdbc:mysql://localhost:3307/clientedb","root","");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/clientedb?serverTimezone=UTC","root","");
 		
 			String consulta = "DELETE FROM tbclientes WHERE id=?";
 			
@@ -145,7 +145,7 @@ public class CRUDCliente {
 			//carregar o drive de comunicação com o banco de dados
 			Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
 			//Chamar o gerenciador de driver
-			con = DriverManager.getConnection("jdbc:mysql://localhost:3307/clientedb","root","");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/clientedb?serverTimezone=UTC","root","");
 			//Vamos criar a consulta para selecionar os clientes por nome
 			String consulta = "Select * from tbcliente where nome=?";
 			pst = con.prepareStatement(consulta);
@@ -180,10 +180,84 @@ public class CRUDCliente {
 		return lista;
 	}
 	public Cliente PesquisarPorId(int id){
-		return null;
+		
+		Cliente cliente = new Cliente();
+		
+		try {
+			//carregar o drive de comunicação com o banco de dados
+			Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+			//Chamar o gerenciador de driver
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3307/clientedb","root","");
+			//Vamos criar a consulta para selecionar os clientes por nome
+			String consulta = "Select * from tbcliente where id=?";
+			pst = con.prepareStatement(consulta);
+			
+			pst.setInt(1, id);
+			
+			//vamos executar a consulta e guardar o resultado na variavel rs
+			rs = pst.executeQuery();
+			//vamos pegar um cliente por vez que esta no rs e adiciona-lo à lista de clientes para, então, retorna-la
+		if(rs.next()) {
+			cliente.setId(rs.getInt(0));
+			cliente.setNome(rs.getString(1));
+			cliente.setEmail(rs.getString(2));
+			cliente.setTelefone(rs.getString(3));
+			cliente.setId(rs.getInt(4));
+		}
+		}//Fim do try
+		catch(SQLException ex) {
+			ex.printStackTrace();
+			
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			try {con.close();} catch(Exception e) {e.printStackTrace();}
+		}
+		
+		return cliente;
 	}
 	
 	public List<Cliente> PesquisarTodos(){
-		return null;
+List<Cliente> lista = new ArrayList<Cliente>();
+		
+		try {
+			//carregar o drive de comunicação com o banco de dados
+			Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+			//Chamar o gerenciador de driver
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3307/clientedb","root","");
+			//Vamos criar a consulta para selecionar os clientes por nome
+			String consulta = "Select * from tbcliente";
+			pst = con.prepareStatement(consulta);
+			
+			
+			
+			//vamos executar a consulta e guardar o resultado na variavel rs
+			rs = pst.executeQuery();
+			//vamos pegar um cliente por vez que esta no rs e adiciona-lo à lista de clientes para, então, retorna-la
+		while(rs.next()) {
+			lista.add(new Cliente(
+					rs.getInt(0),
+					rs.getString(1),
+					rs.getString(2),
+					rs.getString(3),
+					rs.getInt(4)
+					));
+			
+		}//Fim do While
+		}//Fim do try
+		catch(SQLException ex) {
+			ex.printStackTrace();
+			
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			try {con.close();} catch(Exception e) {e.printStackTrace();}
+		}
+		
+		return lista;
 	}
 }
---
+++ src/com/prjcadcliente/view/GerenciarClientes.java
@@ -0,0 +1,176 @@
+package com.prjcadcliente.view;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+import com.prjcadcliente.dominio.Cliente;
+import com.prjcadcliente.persistencia.CRUDCliente;
+
+public class GerenciarClientes extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField txtNome;
+	private JTextField txtEmail;
+	private JTextField txtTelefone;
+	private JTextField txtIdade;
+	private JTable tbClientesCadastro;
+	private Cliente cliente;
+	private CRUDCliente crud;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GerenciarClientes frame = new GerenciarClientes();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public GerenciarClientes() {
+		//Vamos instanciar as classe Cliente e CRUD
+		cliente = new Cliente();
+		crud = new CRUDCliente();
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 459, 601);
+		contentPane = new JPanel();
+		contentPane.setBackground(Color.GRAY);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnCadastrar = new JButton("Cadastrar");
+		btnCadastrar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//Passar os dados do formulário para o objeto cliente
+				cliente.setNome(txtNome.getText());
+				cliente.setEmail(txtEmail.getText());
+				cliente.setTelefone(txtTelefone.getText());
+				cliente.setIdade(Integer.parseInt(txtIdade.getText()));
+				
+				String retorno = crud.cadastrar(cliente);
+				JOptionPane.showMessageDialog(null, retorno);
+				txtNome.setText("");
+				txtEmail.setText("");
+				txtTelefone.setText("");
+				txtIdade.setText("");
+			}
+		});
+		btnCadastrar.setBackground(Color.LIGHT_GRAY);
+		btnCadastrar.setBounds(10, 248, 97, 23);
+		contentPane.add(btnCadastrar);
+		
+		JButton btnPesquisar = new JButton("Pesquisar");
+		btnPesquisar.setBackground(Color.LIGHT_GRAY);
+		btnPesquisar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnPesquisar.setBounds(117, 248, 97, 23);
+		contentPane.add(btnPesquisar);
+		
+		JButton btnDeletar = new JButton("Deletar");
+		btnDeletar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String id = JOptionPane.showInputDialog("Digite o ID do cliente Para Apagar");
+				cliente.setId(Integer.parseInt(id));
+				JOptionPane.showMessageDialog(null, crud.deletar(cliente));
+			}
+		});
+		btnDeletar.setBackground(Color.LIGHT_GRAY);
+		btnDeletar.setBounds(331, 248, 97, 23);
+		contentPane.add(btnDeletar);
+		
+		
+		JButton btnAtualizar = new JButton("Atualizar");
+		btnAtualizar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String id = JOptionPane.showInputDialog("Digite o ID do cliente");
+				
+					//Passar os dados do formulário para o objeto cliente
+					cliente.setNome(txtNome.getText());
+					cliente.setEmail(txtEmail.getText());
+					cliente.setTelefone(txtTelefone.getText());
+					cliente.setIdade(Integer.parseInt(txtIdade.getText()));
+					cliente.setId(Integer.parseInt(id));
+					
+					String retorno = crud.atualizar(cliente);
+					JOptionPane.showMessageDialog(null, retorno);
+					txtNome.setText("");
+					txtEmail.setText("");
+					txtTelefone.setText("");
+					txtIdade.setText("");
+			}
+		});
+		btnAtualizar.setBackground(Color.LIGHT_GRAY);
+		btnAtualizar.setBounds(224, 248, 97, 23);
+		contentPane.add(btnAtualizar);
+		
+		JLabel lblNome = new JLabel("Nome");
+		lblNome.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		lblNome.setBounds(21, 11, 48, 14);
+		contentPane.add(lblNome);
+		
+		JLabel lblEmail = new JLabel("E-mail");
+		lblEmail.setBounds(21, 61, 48, 14);
+		contentPane.add(lblEmail);
+		
+		JLabel lblTelefone = new JLabel("Telefone");
+		lblTelefone.setBounds(21, 117, 97, 14);
+		contentPane.add(lblTelefone);
+		
+		JLabel lblIdade = new JLabel("Idade");
+		lblIdade.setBounds(21, 173, 48, 14);
+		contentPane.add(lblIdade);
+		
+		txtNome = new JTextField();
+		txtNome.setBounds(20, 30, 334, 20);
+		contentPane.add(txtNome);
+		txtNome.setColumns(10);
+		
+		txtEmail = new JTextField();
+		txtEmail.setBounds(20, 86, 334, 20);
+		contentPane.add(txtEmail);
+		txtEmail.setColumns(10);
+		
+		txtTelefone = new JTextField();
+		txtTelefone.setBounds(21, 142, 333, 20);
+		contentPane.add(txtTelefone);
+		txtTelefone.setColumns(10);
+		
+		txtIdade = new JTextField();
+		txtIdade.setBounds(20, 198, 334, 20);
+		contentPane.add(txtIdade);
+		txtIdade.setColumns(10);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 299, 416, 262);
+		contentPane.add(scrollPane);
+		
+		tbClientesCadastro = new JTable();
+		scrollPane.setViewportView(tbClientesCadastro);
+	}
+}
